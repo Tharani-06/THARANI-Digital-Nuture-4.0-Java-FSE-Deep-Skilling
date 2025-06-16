3bb0d590b0ee893f144b721835842a30e9009d61
THARANI T <141557275+Tharani-06@users.noreply.github.com>
2025-06-16 15:43:26 +0530

    Add files via upload

---
+++ week1/Data Structures and Algorithms/Exercise 2: E-commerce Platform Search Function/SearchUtility.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SearchUtility {
+
+    public static Product linearSearch(Product[] products, int targetId) {
+        for (Product p : products) {
+            if (p.getProductId() == targetId) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public static Product binarySearch(Product[] products, int targetId) {
+        int low = 0;
+        int high = products.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midId = products[mid].getProductId();
+
+            if (midId == targetId) {
+                return products[mid];
+            } else if (midId < targetId) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return null;
+    }
+
+    public static void sortByProductId(Product[] products) {
+        Arrays.sort(products, Comparator.comparingInt(Product::getProductId));
+    }
+}
