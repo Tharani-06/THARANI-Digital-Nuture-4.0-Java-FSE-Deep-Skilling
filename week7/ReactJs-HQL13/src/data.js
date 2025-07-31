212de5c51ac6a28b3b05f951947ee9dcab98e20f
THARANI T <141557275+Tharani-06@users.noreply.github.com>
2025-07-31 15:31:22 +0530

    Add files via upload

---
+++ week7/ReactJs-HQL13/src/data.js
@@ -0,0 +1,15 @@
+export const books = [
+  { id: 1, bname: "React Basics", price: 650 },
+  { id: 2, bname: "Deep Dive into Node Js", price: 900 },
+  { id: 3, bname: "Mongo Essentials", price: 750 }
+];
+
+export const blogs = [
+  { id: 1, title: "Node Js Learning", author: "Patric Paul", content: "Welcome to learning Node!" },
+  { id: 2, title: "Installation", author: "Selena Mary", content: "Let's install Node." }
+];
+
+export const courses = [
+  { id: 1, cname: "React", date: "17/04/2025" },
+  { id: 2, cname: "Node", date: "25/05/2025" }
+];
